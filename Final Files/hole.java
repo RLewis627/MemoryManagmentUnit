3dfe7df22ad7eaa46bbd4728bca9823360fd1333
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-05-05 21:45:56 -0700

    Update hole.java

--- Final Files/hole.java
+++ Final Files/hole.java
@@ -1,3 +1,14 @@
+/***************************************************************
+* file: hole.java
+* author: Rachel Lewis
+* class: CS 4310 - Operating Systems
+*
+* assignment: program 3
+* date last modified: 5/3/2020
+*
+* purpose: This file is the class definition for a hole object.
+*
+****************************************************************/ 
 public class hole {
 	private int Size;
 	private int start;
